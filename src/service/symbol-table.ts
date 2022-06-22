dd4b465f00bad3eff0cc4c39f6c668de17f79db6
MamadTvl <mtavallai80@gmail.com>
2022-06-22 11:03:17 +0430

    symbol table action smarter

--- src/service/symbol-table.ts
+++ src/service/symbol-table.ts
@@ -11,26 +11,13 @@ export class SymbolTable implements SymbolTableInterface {
     }
 
     insert(key: string, SymbolNode: SymbolNode): boolean {
-        // if (SymbolNode.isFunction) {
-        //     if (!this.tables.has(key)) {
-        //         this.tables.set(key, new Map());
-        //     } else {
-        //         const symbols = this.tables.get(key) as Map<
-        //             string,
-        //             SymbolNode[]
-        //         >;
-        //         symbols.set(key, [SymbolNode]);
-        //     }
-        // }
         if (this.symbols.has(key)) {
             const nodes = this.symbols.get(key) as SymbolNode[];
             if (nodes.find((node) => node.scope === SymbolNode.scope)) {
-                console.log(
-                    `Semantic Error: ${
+                this.error(
+                    `${
                         SymbolNode.isFunction ? 'Function' : 'identifier'
-                    } ${key} already exists at ${this.lexer.line}:${
-                        this.lexer.column
-                    }`,
+                    } ${key} already exists`,
                 );
                 return false;
             }
@@ -41,25 +28,36 @@ export class SymbolTable implements SymbolTableInterface {
         return true;
     }
 
-    lookup(key: string, scope: string, withError = false): SymbolNode | null {
+    lookup(
+        key: string,
+        scope: string,
+        withError = false,
+        isFunction = false,
+    ): SymbolNode | null {
         const [functionName, scopeNumber] = scope.split('-');
-        if (this.symbols.has(key)) {
-            const nodes = this.symbols.get(key) as SymbolNode[];
-            const node = nodes.find((node) => {
+        const findCallback = (node: SymbolNode) => {
+            if (isFunction) {
+                return node.scope === `${key}-0`;
+            } else {
                 if (node.scope.split('-')[0] === functionName) {
                     if (+node.scope.split('-')[1] <= +scopeNumber) {
                         return node;
                     }
                 }
                 return undefined;
-            });
+            }
+        };
+
+        if (this.symbols.has(key)) {
+            const nodes = this.symbols.get(key) as SymbolNode[];
+            const node = nodes.find(findCallback);
             if (node) {
                 return node;
             }
         }
         withError &&
-            console.log(
-                `Semantic Error: identifier/function ${key} not found at ${this.lexer.line}:${this.lexer.column}`,
+            this.error(
+                `${isFunction ? 'function' : 'identifier'} ${key} not found`,
             );
         return null;
     }
@@ -77,5 +75,11 @@ export class SymbolTable implements SymbolTableInterface {
             }
         }
         return false;
+    }
+
+    error(message: string) {
+        console.log(
+            `Semantic Error: ${message} at ${this.lexer.line}:${this.lexer.column}`,
+        );
     }
 }
